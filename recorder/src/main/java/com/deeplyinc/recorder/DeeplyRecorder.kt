39423a0ce22b5d6718411a1a7186f50eacd4de20
Han Park <han@deeplyinc.com>
2022-11-22 16:25:15 +0900

    feat: change FloatArray as a default return type of start() method

--- recorder/src/main/java/com/deeplyinc/recorder/DeeplyRecorder.kt
+++ recorder/src/main/java/com/deeplyinc/recorder/DeeplyRecorder.kt
@@ -22,31 +22,38 @@ class DeeplyRecorder(
     /** Specify the recording channel, and default value is AudioFormat.CHANNEL_IN_MONO */
     private val channel: Int = AudioFormat.CHANNEL_IN_MONO,
     /**
-     * Specify the buffer size of the recorder, which is the number of audio samples recorded at
-     * one time. If the given size is less than the minimum size of the buffer, it is set to the
-     * minimum size of the buffer. Default value is AudioRecord.getMinBufferSize()
-     */
-    bufferSize: Int? = null
-) {
-    /**
      * Audio format is strongly coupled with the type of buffer.
      * ex) AudioFormat.ENCODING_PCM_16BIT -> ShortArray
      * ex) AudioFormat.ENCODING_PCM_FLOAT -> FloatArray
      * Currently we only support ENCODING_PCM_16BIT. If you need floating point audio samples,
      * you need to scale the short values.
      */
-    private val audioFormat = AudioFormat.ENCODING_PCM_16BIT
+    private val audioFormat: Int = AudioFormat.ENCODING_PCM_16BIT,
+    /**
+     * Specify the buffer size of the recorder, which is the number of audio samples recorded at
+     * one time. If the given size is less than the minimum size of the buffer, it is set to the
+     * (2 * minimum size of the buffer). Default value is (2 * AudioRecord.getMinBufferSize())
+     */
+    bufferSize: Int = 2 * AudioRecord.getMinBufferSize(
+        sampleRate,
+        channel,
+        audioFormat
+    )
+) {
+    init {
+        require(bufferSize > AudioRecord.getMinBufferSize(
+            sampleRate,
+            channel,
+            audioFormat,
+        ))
+    }
+
 
     /**
      * A buffer for storing audio samples recorded from microphone.
      */
-    private var buffer: ShortArray = ShortArray(
-        bufferSize ?: AudioRecord.getMinBufferSize(
-            sampleRate,
-            channel,
-            audioFormat
-        )
-    )
+    private var buffer: FloatArray = FloatArray(bufferSize)
+    private var bufferInShort: ShortArray = ShortArray(bufferSize)
 
     private var audioRecorder: AudioRecord? = null
     private var run = false
@@ -56,8 +63,15 @@ class DeeplyRecorder(
      * be granted before starting the recording.
      */
     @RequiresPermission(Manifest.permission.RECORD_AUDIO)
-    fun start(): Flow<ShortArray> = flow {
-        audioRecorder = AudioRecord(audioSource, sampleRate, channel, audioFormat, buffer.size)
+    fun start(): Flow<FloatArray> = flow {
+        audioRecorder = when (audioFormat) {
+            AudioFormat.ENCODING_PCM_FLOAT -> AudioRecord(audioSource, sampleRate, channel, audioFormat, buffer.size)
+            AudioFormat.ENCODING_PCM_16BIT -> AudioRecord(audioSource, sampleRate, channel, audioFormat, bufferInShort.size)
+            else -> throw UnsupportedEncodingException()
+        }
+        if (audioRecorder?.state == AudioRecord.STATE_UNINITIALIZED) {
+            throw Exception("Failed to initialize AudioRecord")
+        }
         audioRecorder?.let {
             it.startRecording()
             if (it.state == AudioRecord.STATE_UNINITIALIZED) {
@@ -65,7 +79,16 @@ class DeeplyRecorder(
             }
             run = true
             while (run) {
-                it.read(buffer, 0, buffer.size, AudioRecord.READ_BLOCKING)
+                when (audioFormat) {
+                    AudioFormat.ENCODING_PCM_FLOAT -> {
+                        it.read(buffer, 0, buffer.size, AudioRecord.READ_BLOCKING)
+                    }
+                    AudioFormat.ENCODING_PCM_16BIT -> {
+                        it.read(bufferInShort, 0, bufferInShort.size, AudioRecord.READ_BLOCKING)
+                        convertToFloat(bufferInShort, buffer)
+                    }
+                    else -> throw UnsupportedEncodingException()
+                }
                 emit(buffer)
             }
             it.stop()
@@ -88,7 +111,19 @@ class DeeplyRecorder(
     /**
      * @return Audio buffer size, which is the number of audio samples recorded at one time.
      */
-    fun getBufferSize(): Int = buffer.size
+    fun getBufferSize(): Int = bufferInShort.size
+
+    /**
+     * Convert [-32768, 32767] 'short' type data to [-1.0, 1.0] 'float' type data
+     * Warning: this method OVERWRITE the contents of floatArray.
+     */
+    private fun convertToFloat(from: ShortArray, to: FloatArray) {
+        require(from.size == to.size)
+        val size = from.size
+        for (i in 0 until size) {
+            to[i] = from[i] / 32768.0F
+        }
+    }
 
     companion object {
         const val TAG = "DeeplyRecorder"
